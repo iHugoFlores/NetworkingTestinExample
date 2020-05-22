a7ca612c1ebf7a0f60d01e53808b670dd01fcc29
Hugo Flores <hfp.ing2@gmail.com>
2020-05-22 09:19:20 -0400

    Tests Implemented

--- TestableAPITests/TestableAPITests.swift
+++ TestableAPITests/TestableAPITests.swift
@@ -13,6 +13,7 @@ class MockNetworkEngine: NetworkHandler {
     var dataResponse: Data?
     var urlResponse: URLResponse?
     var errorResponse: NetworkError?
+    let mockURL = URL(string: "www.google.com")
     
     func setResponses(data: Data?, urlResponse: URLResponse?, error: NetworkError?) {
         dataResponse = data
@@ -22,6 +23,13 @@ class MockNetworkEngine: NetworkHandler {
     
     func getData(url: URL, _ completion: @escaping completionHandler) {
         completion(dataResponse, urlResponse, errorResponse)
+    }
+    
+    func loadMockJSON() {
+        let bundle = Bundle(for: type(of: self))
+        let fileUrl = bundle.url(forResource: "Albums", withExtension: "json")
+        dataResponse = try! Data(contentsOf: fileUrl!)
+        urlResponse = HTTPURLResponse(url: mockURL!, statusCode: 200, httpVersion: nil, headerFields: nil)
     }
 }
 
@@ -35,12 +43,34 @@ class TestableAPITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        let networkManager = NetworkManager(networkHandler: MockNetworkEngine())
-        networkManager.getData(url: URL(string: "https://jsonplaceholder.typicode.com/albums/1/photos")!) { (result: Result<Album, NetworkError>) in
-            print("This is the result: ", result)
+    func testValidResponse() throws {
+        let mock = MockNetworkEngine()
+        mock.loadMockJSON()
+        let networkManager = NetworkManager(networkHandler: mock)
+        Album.getAlbums(networkManager: networkManager) { (result) in
+            switch result {
+            case .success(let albums):
+                XCTAssertEqual(albums.count, 2, "Number of entries do not match")
+            case .failure:
+                XCTFail("The test should have returned success")
+            }
         }
-        print("Right after")
-        XCTAssertTrue(true)
+    }
+    
+    func testEmptyResponse() throws {
+        let mock = MockNetworkEngine()
+        let networkManager = NetworkManager(networkHandler: mock)
+        Album.getAlbums(networkManager: networkManager) { (result) in
+            switch result {
+            case .success:
+                XCTFail("The test should have returned failure")
+            case .failure(let error):
+                if case NetworkError.emptyResult = error {
+                    XCTAssertTrue(true)
+                } else {
+                    XCTFail("Error was not emptyResult")
+                }
+            }
+        }
     }
 }
